1d2f8404def81e8eb6b71026fbe8e70e859d8aae
ayodeji folarin ajuwon <folarin-_a@qwasar.io>
2023-04-28 21:01:27 +0000

    Previos file

---
+++ functions.h
@@ -0,0 +1,10 @@
+//#ifndef FUNCTIONS_H
+//#define FUNCTIONS_H
+
+// prototype for the my_isdigit function
+//int my_isdigit(char* str);
+
+// prototype for the equality function
+//int equality(char* str1, char* str2);
+
+//#endif // FUNCTIONS_H
---
+++ info.h
@@ -0,0 +1,13 @@
+//#ifndef INFO_H
+//#define INFO_H
+
+// prototype for the start_info function
+//void start_info(void);
+
+// prototype for the endfile function
+//void endfile(void);
+
+// prototype for the commandInput function
+//char* commandInput(void);
+
+//#endif // INFO_H
